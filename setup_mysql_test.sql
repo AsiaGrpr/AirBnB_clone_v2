d51f3dd921e3d8c3784c9dab3058bd8ba7948474
AsiaGrpr <groupierre.asia@gmail.com>
2023-04-06 08:52:38 -0700

    changes task 7

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,9 +4,6 @@
 -- 'hbnb_test' should have SELECT privilege on the database 'performance_schema'
 -- If the database hbnb_test_db or the user 'hbnb_test' already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS hbnb_test@localhost;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
-ALTER USER hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
-
-GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
-ALTER USER hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
